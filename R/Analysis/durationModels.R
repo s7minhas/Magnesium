02eda0461362b971c18655ec76acc4085b8033e9
cassyld <cassyld@gmail.com>
2013-09-29 10:10:26 -0400

    modeling extravaganza

--- R/Analysis/durationModels.R
+++ R/Analysis/durationModels.R
@@ -8,6 +8,7 @@ load('durData.rda')
 ids=data.frame(cbind(unique(aData$targetstate),1:length(unique(aData$targetstate))))
 names(ids)=c('targetstate','fcode')
 aData=merge(aData,ids,by='targetstate',all.x=T)
+
 ############################################################
 # Frailty models
 cmodel1 = coxph(Surv(start, stop, compliance) ~ 
@@ -26,17 +27,82 @@ cmodel2 = coxph(Surv(start, stop, compliance) ~
 summary(cmodel2)
 plot(survfit(cmodel2))
 
+# lagging most things
 cmodel3 = coxph(Surv(start, stop, compliance) ~ 
 	lag1_noS + lag1_polity + lag1_distdata 
-	+ lag1_lgdpCAP + lag1_Internal.Conflict + lag1_edata
-	+ lag1_allydata + lag1_igodata
+	+ lag1_lgdpCAP + lag1_Internal.Conflict + lag1_tdata
+	+ lag1_allydata + lag1_igodata + lag1_religdata + sancRecCnt
 	+ frailty.gaussian(caseid,sparse=FALSE)
 	, data=aData)
 summary(cmodel3)
-plot(survfit(cmodel3))
 
-duration ~ Internal.Conflict + gdpCAP + polity,
-	c ~ noS + distdata + sancRecCnt + edata + allydata + igodata +religdata,
+# not lagging everything
+# incudes: controls + senders & distance hypo + net hypo
+# ***pretty interesting results
+cmodel4 = coxph(Surv(start, stop, compliance) ~ 
+	noS + lag1_polity + distdata 
+	+ lag1_lgdpCAP + Internal.Conflict + tdata
+	+ allydata + igodata + sancRecCnt + religdata
+	+ frailty.gaussian(caseid,sparse=FALSE)
+	, data=aData)
+summary(cmodel4)
+
+# includes: controls + senders & distance hypo + net hypo
+	# diff religious measure, also influential but not big change
+cmodel5 = coxph(Surv(start, stop, compliance) ~ 
+	noS + lag1_polity + distdata 
+	+ lag1_lgdpCAP + Internal.Conflict + tdata
+	+ allydata + igodata + sancRecCnt + Creligdata
+	+ frailty.gaussian(caseid,sparse=FALSE)
+	, data=aData)
+summary(cmodel5)
+
+#controls + senders & distance hypo + net hypos 
+	# + diff polity measures (polcom, xropen, do not change much)
+	# *** but polconii does!
+cmodel6 = coxph(Surv(start, stop, compliance) ~ 
+	noS  + distdata + polconiii
+	+ lag1_lgdpCAP + Internal.Conflict + tdata
+	+ allydata + igodata + sancRecCnt + religdata
+	+ frailty.gaussian(caseid,sparse=FALSE)
+	, data=aData)
+summary(cmodel6)
+
+#controls + senders & distance hypo + net hypo 
+	# diff conflict indicators, domestic9 & civ war didnt do much
+cmodel7 = coxph(Surv(start, stop, compliance) ~ 
+	noS  + distdata + polconiii
+	+ lag1_lgdpCAP + domestic9 + tdata
+	+ allydata + igodata + sancRecCnt + religdata
+	+ frailty.gaussian(caseid,sparse=FALSE)
+	, data=aData)
+summary(cmodel7)
+plot(survfit(cmodel7))
+
+#same as model 6 but with interaction polconiii:noS
+	#also tried with polity interaction but wasn't super interesting
+	#and neither is this interaction
+cmodel8 = coxph(Surv(start, stop, compliance) ~ 
+	noS + polconiii + distdata 
+	+ lag1_lgdpCAP + Internal.Conflict + tdata
+	+ allydata + igodata + sancRecCnt + religdata
+	+ polconiii:noS
+	+ frailty.gaussian(caseid,sparse=FALSE)
+	, data=aData)
+summary(cmodel8)
+plot(survfit(cmodel8))
+
+#same as model 6 but with interaction internalconflict:noS
+cmodel9 = coxph(Surv(start, stop, compliance) ~ 
+	noS + polconiii + distdata 
+	+ lag1_lgdpCAP + Internal.Conflict + tdata
+	+ allydata + igodata + sancRecCnt + religdata
+	+ Internal.Conflict:noS
+	+ frailty.gaussian(caseid,sparse=FALSE)
+	, data=aData)
+summary(cmodel9)
+plot(survfit(cmodel9))
+
 ############################################################
 
 
@@ -80,7 +146,7 @@ model1 = spdur(duration ~ lgdpCAP,
 	last=full$end.spell, data=full, test=full, distr='weibull', iter=300)
 
 model2 = spdur(duration ~ Internal.Conflict + gdpCAP + polity,
-	c ~ noS + distdata + sancRecCnt + edata + allydata + igodata +religdata,
+	c ~ noS + distdata + sancRecCnt + tdata + allydata + igodata +religdata,
 	last=full$end.spell, data=full, test=full, distr='weibull', iter=300)
 
 predProbs <- predictSPD(model1, pred)
