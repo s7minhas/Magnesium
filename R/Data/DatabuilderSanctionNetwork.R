7d3088059a49f05f67cf5afecbfc104ca78b07cb
hermes829 <hermes829@gmail.com>
2013-10-01 08:26:53 -0400

    rebuilt sanc net data

--- R/Data/DatabuilderSanctionNetwork.R
+++ R/Data/DatabuilderSanctionNetwork.R
@@ -3,35 +3,33 @@ source('/Users/janus829/Desktop/Research/Magnesium/R/Setup.R')
 # Load sanction network Data
 setwd(pathData)
 load('sanctionData.rda')
-senders=paste('sender',1:5,'_ccode',sep='')
-sdata=sanctionDataFinal[,c('targetstate',senders,'startyear','endyear','caseid')]
+sendIDs=paste('sender',1:5,'_ccode',sep='')
+sdata=sanctionDataFinal[,c('targetstate',sendIDs,'startyear','endyear','caseid')]
 
 # Setting up list of country names in existence for time period of analysis
 setwd(pathPData)
 load('panel.rda')
 years=seq(1960, 2005, 1)
-cntryYr=lapply(years, function(x) FUN=panel[panel$year==x,'ccode'])
+ctryYr=lapply(years, function(x) FUN=panel[panel$year==x,'ccode'])
 
-sanctionDyadData=list()
+smatList=list()
 for(ii in 1:length(years)){
-	temp=sdata[years[ii]>=sdata$startyear & years[ii]<=sdata$endyear2,]  
+	slice=sdata[which(years[ii]>=sdata$startyear & years[ii]<=sdata$endyear),]  
 	
-	cntry=cntryYr[[ii]]
-	temp3=matrix(0, nrow=length(cntry), ncol=length(cntry), dimnames=list(cntry, cntry))
-	
-	for(jj in 1:ncol(t(temp))){
-		senders=as.character(as.vector(t(temp)[3:7,jj]))
-		senderPrim=as.character(as.vector(t(temp)[8,jj]))
-		target=as.character(as.vector(t(temp)[9,jj]))
+	ctrs=ctryYr[[ii]]
+	smat=matrix(0, nrow=length(ctrs), ncol=length(ctrs), dimnames=list(ctrs, ctrs))
 
-		temp2=matrix(0, nrow=length(cntry), ncol=length(cntry), dimnames=list(cntry, cntry))
-		temp2[senders, target]=1
-		temp2[senderPrim, target]=1
-		temp3=temp3 + temp2
+	for(jj in 1:nrow(slice)){
+		sndrs=slice[jj,sendIDs]; sndrs=as.character(sndrs[!is.na(sndrs)])
+		sndrs=sndrs[which(sndrs!=setdiff(sndrs,rownames(smat)))]
+		trgt=slice[jj,'targetstate']; trgt=as.character(trgt)
+		if(length(setdiff(trgt,rownames(smat)))==0){
+				smat2=matrix(0, nrow=length(ctrs), ncol=length(ctrs), dimnames=list(ctrs, ctrs))
+				smat2[sndrs, trgt]=1
+				smat=smat + smat2}
 	}
-
-	sanctionDyadData[[ii]]=temp3
+	smatList[[ii]]=smat
 }
 
 setwd(pathData)
-save(sanctionDyadData, file='sanction.rda')
+save(smatList, file='sanction.rda')
