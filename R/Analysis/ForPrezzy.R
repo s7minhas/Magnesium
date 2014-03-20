f71415a24b673a60dfbffb88a8f5c3638c83fe24
hermes829 <hermes829@gmail.com>
2014-03-20 09:08:25 -0400

    cleaning

--- R/Analysis/ForPrezzy.R
+++ R/Analysis/ForPrezzy.R
@@ -10,47 +10,54 @@ ids=data.frame(cbind(unique(aData$targetstate),1:length(unique(aData$targetstate
 names(ids)=c('targetstate','fcode')
 aData=merge(aData,ids,by='targetstate',all.x=T)
 
-# Var mods
-aData$interaction=aData$lag1_polconiii*aData$noS
-
 # Variable key
 varDef = cbind (  
-	c( 'noS', 'interaction', 'distdata', 'tdata', 'allydata', 'igodata', 'Creligdata',
-	 'lag1_sancRecCnt', 'lag1_polconiii', 'lag1_lgdpCAP', 'lag1_Internal.Conflict'),
-	c( 'Number of Senders$_{j,t}$', 'Senders$_{j,t}$*Constraints$_{i,t-1}$',
+	c( 'noS', 'distdata', 'tdata', 'allydata', 'igodata', 'Creligdata',
+	 'lag1_sancSenCnt', 'lag1_sancRecCnt', 
+	 'lag1_polconiii', 'lag1_lgdpCAP', 'lag1_Internal.Conflict'),
+	c( 'Number of Senders$_{j,t}$', 
 	'Distance$_{j,t}$', 'Trade$_{j,t}$', 'Ally$_{j,t}$', 'IGOs$_{j,t}$', 
-	'Religion$_{j,t}$', "Sanc. Rec'd$_{i,t}$", 'Constraints$_{i,t}$',
-	'Ln(GDP per capita)$_{i,t}$', 'Internal Stability$_{i,t}$' )
+	'Religion$_{j,t}$', 
+	"Sanc. Sent$_{j,t-1}$", "Sanc. Rec'd$_{i,t-1}$",
+	'Constraints$_{i,t-1}$',
+	'Ln(GDP per capita)$_{i,t-1}$', 'Internal Stability$_{i,t-1}$' )
 	)
 
-# not lagging everything
-# incudes: controls + senders & distance hypo + net hypo
-# ***pretty interesting results
-cmodel4 = coxph(Surv(start, stop, compliance) ~ 
-	noS + distdata + tdata + allydata + igodata + Creligdata 
-	+ lag1_sancSenCnt
-	+ lag1_sancRecCnt + lag1_polconiii + lag1_Internal.Conflict
+# Only state-specific non-network/rel. measures
+model1 = coxph(Surv(start, stop, compliance) ~ 
+	lag1_polconiii + lag1_Internal.Conflict
 	+ lag1_lgdpCAP + lag1_gdpGR
 	, data=aData)
-temp=na.omit(aData[,c('caseid',names(cmodel4$coefficients))])
-length(unique(temp$caseid))
 
-m1Tab=summary(cmodel4)$coefficients[,c('coef','se(coef)','Pr(>|z|)')]
-rownames(m1Tab)=varDef[ match(rownames(m1Tab),varDef[,1]) , 2 ]
-m1Tab=xtable(m1Tab)
-setwd(pathGraphics)
-# save(m1Tab, file='mod1.tex')
+# Only network/rel. measures
+model2 = coxph(Surv(start, stop, compliance) ~ 
+	noS + distdata + tdata + allydata + igodata + Creligdata 
+	+ lag1_sancSenCnt + lag1_sancRecCnt
+	, data=aData)
 
-m2Tab=summary(cmodel8)$coefficients[,c('coef','se(coef)','Pr(>|z|)')]
-rownames(m2Tab)=varDef[ match(rownames(m2Tab),varDef[,1]) , 2 ]
-m2Tab=xtable(m2Tab)
-setwd(pathGraphics)
-# save(m2Tab, file='mod2.tex')
+# "network/relational" variables unlagged
+model3 = coxph(Surv(start, stop, compliance) ~ 
+	noS + distdata + tdata + allydata + igodata + Creligdata 
+	+ lag1_sancSenCnt + lag1_sancRecCnt
+	+ lag1_polconiii + lag1_Internal.Conflict
+	+ lag1_lgdpCAP + lag1_gdpGR
+	, data=aData)
+
+setwd(pathTex)
+durTables=durTable(list(model1, model2, model3), varDef)
+print.xtable( xtable(durTables, align='llccc', 
+	caption='Results of duration models with time varying covariates estimated using Cox Proportional Hazards. Standard errors in parentheses. $^{**}$ and $^{*}$ indicate significance at $p< 0.05 $ and $p< 0.10 $, respectively.'
+	),
+	include.rownames=FALSE, sanitize.text.function=identity,
+	hline.after=c(0,0,12,16,nrow(varDef)*2, nrow(varDef)*2+3,nrow(varDef)*2+3),
+	size='normalsize',
+	file='durModelResults.tex'
+	)
 
 ###
 # Vars to generate survival plots for:
 	# noS, distance, ally, igo, religion
-simModel=cmodel8
+simModel=model3
 pcolors=append(brewer.pal(9,'Reds')[8],brewer.pal(9,'Blues')[8])
 vrfn=function(x){c(min(x,na.rm=T),max(x,na.rm=T))}
 
