b47d402ab11c73b43e6705be8f08a58a0ff5f0e5
hermes829 <hermes829@gmail.com>
2014-07-03 15:47:52 -0400

    cumul and unit auc levels

--- R/Analysis/durModelsPerf.R
+++ R/Analysis/durModelsPerf.R
@@ -9,59 +9,76 @@ load('durModels.rda')
 ###############################################################
 
 ###############################################################
-# Performance
+# Using survauc
 idVars=idVars[c(1:10,17:19)]
-M1Data=na.omit(modData[,c(idVars,varDef[7:nrow(varDef),1]) ])
-M2Data=na.omit(modData[,c(idVars,varDef[3:nrow(varDef),1]) ])
-MfinData=na.omit(modData[,c(idVars,varDef[,1]) ])
+m1Data=na.omit(modData[,c(idVars,varDef[7:nrow(varDef),1]) ])
+m2Data=na.omit(modData[,c(idVars,varDef[3:nrow(varDef),1]) ])
+mFData=na.omit(modData[,c(idVars,varDef[,1]) ])
+times=seq(0,25,5)
 
 aucM1=AUC.cd(
-	Surv(M1Data$start, M1Data$stop, M1Data$compliance), 
-	Surv(M1Data$start, M1Data$stop, M1Data$compliance), 
+	Surv(m1Data$start, m1Data$stop, m1Data$compliance), 
+	Surv(m1Data$start, m1Data$stop, m1Data$compliance), 
 	predict(model1),
 	predict(model1),
-	times=seq(5,50,5)
+	times=times
 	)
 
 aucM2=AUC.cd(
-	Surv(M2Data$start, M2Data$stop, M2Data$compliance), 
-	Surv(M2Data$start, M2Data$stop, M2Data$compliance), 
+	Surv(m2Data$start, m2Data$stop, m2Data$compliance), 
+	Surv(m2Data$start, m2Data$stop, m2Data$compliance), 
 	predict(model2),
 	predict(model2),
-	times=seq(5,50,5)
+	times=times
 	)
 
 aucFinal=AUC.cd(
-	Surv(MfinData$start, MfinData$stop, MfinData$compliance), 
-	Surv(MfinData$start, MfinData$stop, MfinData$compliance), 
+	Surv(mFData$start, mFData$stop, mFData$compliance), 
+	Surv(mFData$start, mFData$stop, mFData$compliance), 
 	predict(modelFinal),
 	predict(modelFinal),
-	times=seq(5,50,5)
+	times=times
 	)
 
-print(aucM1)
-print(aucM2)
-print(aucFinal)
+print(aucM1$iauc); print(aucM2$iauc); print(aucFinal$iauc)
 
 plot(aucFinal, col='black')
-lines(aucM1$times, aucM1$auc, col='red')
-lines(aucM2$times, aucM2$auc, col='blue')
+lines(aucM1$times, aucM1$auc, col='darkgreen')
+lines(aucM2$times, aucM2$auc, col='darkblue')
+###############################################################
 
-# ROC plots
-predts <- predict(model1, type = "risk")
-perfMod1 <- tdrocc(x = predts, surv.time = M1Data[, "slength"],
-	surv.event = M1Data[, "compliance"], time = 5, na.rm = TRUE)
-predts <- predict(model2, type = "risk")
-perfMod2 <- tdrocc(x = predts, surv.time = M2Data[, "slength"],
-	surv.event = M2Data[, "compliance"], time = 5, na.rm = TRUE)
-predts <- predict(modelFinal, type = "risk")
-perfFinal <- tdrocc(x = predts, surv.time = MfinData[, "slength"],
-	surv.event = MfinData[, "compliance"], time = 5, na.rm = TRUE)
+###############################################################
+# Using survcomp
+# Concordance index
+predM1 <- predict(model1, type = "risk")
+predM2 <- predict(model2, type = "risk")
+predMF <- predict(modelFinal, type = "risk")
+
+conc1 = concordance.index(x = predM1, surv.time = m1Data[, "slength"],
+	surv.event = m1Data[, "compliance"], method = "noether", na.rm = TRUE)
+conc2 = concordance.index(x = predM2, surv.time = m2Data[, "slength"],
+	surv.event = m2Data[, "compliance"], method = "noether", na.rm = TRUE)
+concF = concordance.index(x = predMF, surv.time = mFData[, "slength"],
+	surv.event = mFData[, "compliance"], method = "noether", na.rm = TRUE)
+
+print(conc1[1:5]); print(conc2[1:5]); print(concF[1:5])
+
+# ROC plots with survcomp
+sTime = mean(unique(modData$slength))
+
+perfMod1 <- tdrocc(x = predM1, surv.time = m1Data[, "slength"],
+	surv.event = m1Data[, "compliance"], time = 24, na.rm = TRUE)
+perfMod2 <- tdrocc(x = predM2, surv.time = m2Data[, "slength"],
+	surv.event = m2Data[, "compliance"], time = 24, na.rm = TRUE)
+perfFinal <- tdrocc(x = predMF, surv.time = mFData[, "slength"],
+	surv.event = mFData[, "compliance"], time = 24, na.rm = TRUE)
+
+print(perfMod1$AUC); print(perfMod2$AUC); print(perfFinal$AUC)
 
 plot(x = 1 - perfFinal$spec, y = perfFinal$sens, type = "l",
 	xlab = "1 - specificity", ylab = "sensitivity", xlim = c(0,1),
 	 ylim = c(0, 1), main = "Time-dependent ROC curve\nat 5 years")
-lines(x = 1 - perfMod1$spec, y = perfMod1$sens, col='green')
+lines(x = 1 - perfMod1$spec, y = perfMod1$sens, col='darkblue')
 lines(x = 1 - perfMod2$spec, y = perfMod2$sens, col='darkgreen')
 lines(x = c(0, 1), y = c(0, 1), lty = 3, col = "red")
 ###############################################################
