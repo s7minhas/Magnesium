abafcd54b5148618073b9cc76e963f15a164c419
hermes829 <hermes829@gmail.com>
2014-08-02 16:00:27 -0400

    updates to charts in paper, need to write up cross val anaylsis

--- R/Analysis/crossval.R
+++ R/Analysis/crossval.R
@@ -18,7 +18,7 @@ aData=merge(aData,ids,by='targetstate',all.x=T)
 ###############################################################
 # Splitting up dataframe into random subsets of cases
 caseid=unique(aData$caseid)
-set.seed(6886); rands=sample(rep(1:10, 1000)[1:length(caseid)])
+set.seed(6886); rands=sample(rep(1:6, 1000)[1:length(caseid)])
 idsRand=data.frame(cbind(caseid, rands))
 aData=merge(aData, idsRand, by='caseid', all.x=T)
 ###############################################################
@@ -51,60 +51,74 @@ modData=aData[, c( idVars, varDef[,1] )]
 
 ###############################################################
 # Inputs
+times=seq(1,16,1)
 idVars=idVars[c(1:10,17:19)]
 m1V=which(varDef[,1] =='lag1_polity2')
 m2V=which(varDef[,1]=='noS')
 m1F=formula(Surv(start, stop, compliance) ~ lag1_polity2 + lag1_lgdpCAP + lag1_gdpGR + lag1_lpopulation + lag1_domSUM)
 m2F=formula(Surv(start, stop, compliance) ~ noS + Ddistdata + lag1_tdata + lag1_allydata + lag1_polity2 + lag1_lgdpCAP + lag1_gdpGR + lag1_lpopulation + lag1_domSUM)
-m3F=formula(Surv(start,stop,compliance) ~ lag1_uData + lag1_SuData2 + noS + Ddistdata + lag1_tdata + lag1_allydata + lag1_polity2 + lag1_lgdpCAP + lag1_gdpGR + lag1_lpopulation + lag1_domSUM)
+mFF=formula(Surv(start,stop,compliance) ~ lag1_uData + lag1_SuData2 + noS + Ddistdata + lag1_tdata + lag1_allydata + lag1_polity2 + lag1_lgdpCAP + lag1_gdpGR + lag1_lpopulation + lag1_domSUM)
 
-# set up null dataframe for auc stats
+# Run k-fold cross val with validation
+ggAUC=NULL
+for(ii in 1:length(unique(modData$rands))){
+	train=modData[which(!modData$rands %in% ii),]
+	test=modData[which(modData$rands %in% ii),]
 
-# first subset to new train and test out of 10 available
-# then run m1, m2, and m3 for each
-# then run AUC.cd stats
-# then save to dataframe
+	m1DataTrain=na.omit(train[,c(idVars,varDef[m1V:nrow(varDef),1]) ])
+	m2DataTrain=na.omit(train[,c(idVars,varDef[m2V:nrow(varDef),1]) ])
+	mFDataTrain=na.omit(train[,c(idVars,varDef[,1]) ])
 
-times=seq(1,16,1)
+	m1DataTest=na.omit(test[,c(idVars,varDef[m1V:nrow(varDef),1]) ])
+	m2DataTest=na.omit(test[,c(idVars,varDef[m2V:nrow(varDef),1]) ])
+	mFDataTest=na.omit(test[,c(idVars,varDef[,1]) ])
+
+	modTrain1 = coxph(m1F, data=m1DataTrain)
+	modTrain2 = coxph(m2F, data=m2DataTrain)
+	modTrainF=coxph(mFF, data=mFDataTrain)
+
+	aucM1=AUC.cd(
+		Surv(m1DataTrain$start, m1DataTrain$stop, m1DataTrain$compliance), 
+		Surv(m1DataTest$start, m1DataTest$stop, m1DataTest$compliance), 
+		predict(modTrain1),
+		predict(modTrain1, newdata=m1DataTest),
+		times=times
+		)
+
+	aucM2=AUC.cd(
+		Surv(m2DataTrain$start, m2DataTrain$stop, m2DataTrain$compliance), 
+		Surv(m2DataTest$start, m2DataTest$stop, m2DataTest$compliance), 
+		predict(modTrain2),
+		predict(modTrain2, newdata=m2DataTest),
+		times=times
+		)
+
+	aucMF=AUC.cd(
+		Surv(mFDataTrain$start, mFDataTrain$stop, mFDataTrain$compliance), 
+		Surv(mFDataTest$start, mFDataTest$stop, mFDataTest$compliance), 
+		predict(modTrainF),
+		predict(modTrainF, newdata=mFDataTest),
+		times=times
+		)
+
+	aucData=data.frame( rbind(
+		cbind(aucM1$times, aucM1$auc, 'Model 1\\qquad\\qquad'),
+		cbind(aucM2$times, aucM2$auc, 'Model 2\\qquad\\qquad'),
+		cbind(aucMF$times, aucMF$auc, 'Model 3')
+		) )
+	aucData$X1=numSM(aucData$X1); aucData$X2=numSM(aucData$X2)
+
+	ggAUC=rbind(ggAUC,cbind(aucData, Fold=ii))
+}
 ###############################################################
 
 ###############################################################
-# Cumulative AUC
-aucM1=AUC.cd(
-	Surv(m1DataTrain$start, m1DataTrain$stop, m1DataTrain$compliance), 
-	Surv(m1DataTest$start, m1DataTest$stop, m1DataTest$compliance), 
-	predict(modTrain1),
-	predict(modTrain1, newdata=m1DataTest),
-	times=times
-	)
-
-aucM2=AUC.cd(
-	Surv(m2DataTrain$start, m2DataTrain$stop, m2DataTrain$compliance), 
-	Surv(m2DataTest$start, m2DataTest$stop, m2DataTest$compliance), 
-	predict(modTrain2),
-	predict(modTrain2, newdata=m2DataTest),
-	times=times
-	)
-
-aucMF=AUC.cd(
-	Surv(mFDataTrain$start, mFDataTrain$stop, mFDataTrain$compliance), 
-	Surv(mFDataTest$start, mFDataTest$stop, mFDataTest$compliance), 
-	predict(modTrainF),
-	predict(modTrainF, newdata=mFDataTest),
-	times=times
-	)
-
-print(aucM1$iauc); print(aucM2$iauc); print(aucMF$iauc)
-
-ggAUC=data.frame( rbind(
-	cbind(aucM1$times, aucM1$auc, 'Model 1\\qquad\\qquad'),
-	cbind(aucM2$times, aucM2$auc, 'Model 2\\qquad\\qquad'),
-	cbind(aucMF$times, aucMF$auc, 'Model 3')
-	) )
-ggAUC$X1=numSM(ggAUC$X1); ggAUC$X2=numSM(ggAUC$X2)
+# Plotting
+ggAUC$Fold=paste0('Fold ', ggAUC$Fold)
 
 pgg=ggplot(ggAUC, aes(x=X1, y=X2, color=X3, group=X3))
 pgg=pgg+geom_line(aes(linetype=X3),lwd=1,color='black')
+pgg=pgg+facet_wrap(~Fold, scales="free_y")
 pgg=pgg+scale_x_continuous(breaks=seq(0,15,3),limits=c(0,16))
 pgg=pgg+scale_y_continuous(breaks=seq(0.6,1,0.1),limits=c(0.59,1.01))
 pgg=pgg+xlab('Time (years)')+ylab('Time-dependent AUC')
@@ -114,13 +128,17 @@ pgg=pgg+theme(legend.position='top', legend.title=element_blank(),
       axis.line = element_line(color = 'black'),
       axis.title.y=element_text(vjust=1))
 pgg
+setwd(pathTex)
+tikz(file='crossvalPerf.tex', height=5, width=7, standAlone=F)
+pgg
+dev.off()
 ###############################################################
 
 ###############################################################
 # Run models on subsets
 models=list()
 for( ii in 1:length(unique(modData$rands)) ){
-	slice=modData[which(!modData$rands %in% rands[ii]),]
+	slice=modData[which(!modData$rands %in% ii),]
 	modelFinal=coxph(Surv(start,stop,compliance) ~
 		lag1_uData + lag1_SuData2 
 		+ noS + Ddistdata + lag1_tdata + lag1_allydata
@@ -193,7 +211,7 @@ coefp = coefp + theme(legend.position='none', legend.title=element_blank(),
     axis.text.x=element_text(angle=45,hjust=1))
 coefp
 setwd(pathTex)
-tikz(file='crossval.tex', height=3, width=7, standAlone=F)
+tikz(file='crossvalCoef.tex', height=3, width=7, standAlone=F)
 coefp
 dev.off()
 ##############################################################
