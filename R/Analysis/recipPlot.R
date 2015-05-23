d6c5358d9e1d1e8535e33a51ee3688f1c0d517f9
hermes829 <hermes829@gmail.com>
2015-05-22 23:48:04 -0400

    prelim comp recip plot

---
+++ R/Analysis/recipPlot.R
@@ -0,0 +1,60 @@
+##################################################
+# Purpose: create a recip network plot
+# CD & SM
+
+if(Sys.info()["user"]=="janus829" | Sys.info()["user"]=="s7m"){
+source('~/Research/Magnesium/R/Setup.R');
+source('~/Research/Magnesium/R/Analysis/SRM.R');
+load('~/Research/Magnesium/R/Data/BuildingPanelData/panel.rda')
+}
+
+if(Sys.info()["user"]=="cassydorff"){
+source('/Users/cassydorff/ProjectsGit/Magnesium/R/Setup.R');
+load('/Users/cassydorff/ProjectsGit/Magnesium/R/Data/BuildingPanelData/panel.rda');
+source('/Users/cassydorff/ProjectsGit/Magnesium/R/Analysis/SRM.R')
+}
+###################################################
+
+###################################################
+# Load SRM network data
+setwd(pathData)
+load('compSRM.rda')
+###################################################
+
+###################################################
+# Choose comp network to plot
+comp = ueffect$'2010'
+
+# Set node names
+rownames(comp) = colnames(comp) = panel$cname[match(rownames(comp), panel$ccode)]
+rownames(comp) = colnames(comp) = countrycode(rownames(comp), 'country.name', 'iso3c')
+na = which(is.na(rownames(comp)))
+rownames(comp)[na] = colnames(comp)[na] = 'KOS'
+
+# Pick most active countries
+topS=names(sort(rowSums(abs(comp)), decreasing=TRUE)[1:5])
+topR=names(sort(colSums(abs(comp)), decreasing=TRUE)[1:5])
+top = unique(c(topS, topR))
+compSub = comp[top, top]
+
+# Create igraph object
+g = graph.adjacency(compSub, mode='directed', diag=FALSE, weighted=TRUE)
+
+# Define extra attributes
+E(g)$color = ifelse(E(g)$weight<0, "tomato", "lightblue")
+V(g)$weight = colSums(abs(compSub))
+
+# Plot
+set.seed(6886)
+curves = autocurve.edges2(g)
+plot.igraph(g, 
+	layout=layout.circle,	
+	vertex.color='lightgrey', 
+	vertex.label.cex=(sqrt(V(g)$weight) + 4)/19,
+	vertex.size=sqrt(V(g)$weight) + 4,
+	edge.arrow.size=0.5,
+	edge.width=abs(E(g)$weight)/3,
+	edge.curved = curves,
+	asp=FALSE
+	)
+###################################################s
--- R/Setup.R
+++ R/Setup.R
@@ -426,3 +426,30 @@ riskRatio=function(sims, model, data, var, seed=6886){
 	ci=c( mean( exp( log( hr ) - 1.96*bse ) ), mean( exp( log( hr ) + 1.96*bse ) )) # Conf int
 	matrix( c(hr, ci), ncol=3, dimnames=list(var,c('mean','lci','uci')) ) # Output
 }
+
+# Curve edges in igraph
+# http://stackoverflow.com/questions/16875547/using-igraph-how-to-force-curvature-when-arrows-point-in-opposite-directions
+autocurve.edges2 <-function (graph, start = 0.5)
+{
+    cm <- count.multiple(graph)
+    mut <-is.mutual(graph)  #are connections mutual?
+    el <- apply(get.edgelist(graph, names = FALSE), 1, paste,
+        collapse = ":")
+    ord <- order(el)
+    res <- numeric(length(ord))
+    p <- 1
+    while (p <= length(res)) {
+        m <- cm[ord[p]]
+        mut.obs <-mut[ord[p]] #are the connections mutual for this point?
+        idx <- p:(p + m - 1)
+        if (m == 1 & mut.obs==FALSE) { #no mutual conn = no curve
+            r <- 0
+        }
+        else {
+            r <- seq(-start, start, length = m)
+        }
+        res[ord[idx]] <- r
+        p <- p + m
+    }
+    res
+}
