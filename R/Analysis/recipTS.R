3221e58e144d258ef8c04949b52a87a2f3d2d7c5
hermes829 <hermes829@gmail.com>
2016-03-29 13:29:24 -0700

    time series plots

---
+++ R/Analysis/recipTS.R
@@ -0,0 +1,65 @@
+##################################################
+# Purpose: create a recip network plot
+# CD & SM
+
+if(Sys.info()["user"]=="janus829" | Sys.info()["user"]=="s7m"){
+source('~/Research/Magnesium/R/Setup.R');
+source('~/Research/Magnesium/R/Data/SRM.R');
+load('~/Research/Magnesium/R/Data/BuildingPanelData/panel.rda')
+}
+
+if(Sys.info()["user"]=="cassydorff"){
+source('/Users/cassydorff/ProjectsGit/Magnesium/R/Setup.R');
+load('/Users/cassydorff/ProjectsGit/Magnesium/R/Data/BuildingPanelData/panel.rda');
+source('/Users/cassydorff/ProjectsGit/Magnesium/R/Data/SRM.R')
+}
+###################################################
+
+###################################################
+# Load SRM network data
+
+# If compliance = TRUE then compliance recip network plots are generated
+## if false then sanction recip network plots
+compliance=TRUE
+
+# To print plots set to true
+printPlot=TRUE
+
+setwd(pathData)
+if(compliance){ 
+	load('compSRM.rda') } else { 
+	load('sancSRM.rda'); ueffect=Sueffect }
+ueffect = ueffect[-length(ueffect)]
+###################################################
+
+###################################################
+# Turn into dyadic dataframe
+dat = do.call('rbind', lapply(ueffect, melt) )
+dat$year = rownames(dat) %>% numSM(.) %>% round(.,0)
+dat = dat[which(dat$X1 != dat$X2),]
+names(dat)[1:2] = c('ccodeS','ccodeR') ; rownames(dat) = NULL
+
+# Add abbreviation column to panel dataset
+panel$abb = countrycode(panel$cname, 'country.name', 'iso3c')
+panel$abb[panel$cname=='German Democratic Republic'] = 'GDR'
+panel$abb[panel$cname=='Zanzibar'] = 'ZNZ'
+panel$abb[panel$cname=='KOSOVO'] = 'KOS'
+
+# Add abbreviation labels to dat
+dat$abbS = panel$abb[match(dat$ccodeS, panel$ccode)]
+dat$abbR = panel$abb[match(dat$ccodeR, panel$ccode)]
+###################################################
+
+###################################################
+# Determine pairs to plot
+dat$id = paste(dat$ccodeS, dat$ccodeR, sep='_')
+datVar = summaryBy(value ~ id, FUN=sd, data=dat) %>% na.omit()
+datVar = datVar[which(datVar$value.sd > 0.5),]
+topVar = datVar[order(datVar$value.sd, decreasing=TRUE),][1:10,]
+###################################################
+
+###################################################
+# Plot
+ggData = dat[which(dat$id %in% topVar$id),]
+ggplot(ggData, aes(x=year, y=value, color=id)) + geom_line()
+###################################################
