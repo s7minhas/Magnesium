ca3480dffb1f6768cf7233ba8bc55d78fbb4ab3b
hermes829 <hermes829@gmail.com>
2016-03-30 11:17:43 -0700

    modif recip ts

--- R/Analysis/recipTS.R
+++ R/Analysis/recipTS.R
@@ -48,6 +48,7 @@ panel$abb[panel$cname=='KOSOVO'] = 'KOS'
 # Add abbreviation labels to dat
 dat$abbS = panel$abb[match(dat$ccodeS, panel$ccode)]
 dat$abbR = panel$abb[match(dat$ccodeR, panel$ccode)]
+dat$label = paste(dat$abbS, dat$abbR, sep=' & ')
 ###################################################
 
 ###################################################
@@ -55,11 +56,22 @@ dat$abbR = panel$abb[match(dat$ccodeR, panel$ccode)]
 dat$id = paste(dat$ccodeS, dat$ccodeR, sep='_')
 datVar = summaryBy(value ~ id, FUN=sd, data=dat) %>% na.omit()
 datVar = datVar[which(datVar$value.sd > 0.5),]
-topVar = datVar[order(datVar$value.sd, decreasing=TRUE),][1:10,]
+###################################################
+
+###################################################
+# Add recip rel
+ggData = dat[which(dat$id %in% datVar$id),]
+dat$idYr = paste(dat$id, dat$year, sep='_')
+ggData$idRev = paste(ggData$ccodeR, ggData$ccodeS, sep='_')
+ggData$idRevYr = paste(ggData$ccodeR, ggData$ccodeS, ggData$year, sep='_')
+ggData$valueRev = dat$value[match(ggData$idRevYr, dat$idYr)]
+
+# cleanup
+ggData = ggData[which(!ggData$id %in% ggData$idRev),]
+ggData = melt(ggData[,c('label','year','value','valueRev')], id=c('label','year'))
 ###################################################
 
 ###################################################
 # Plot
-ggData = dat[which(dat$id %in% topVar$id),]
-ggplot(ggData, aes(x=year, y=value, color=id)) + geom_line()
+ggplot(ggData, aes(x=year, y=value, color=variable)) + geom_line() + facet_wrap(~label)
 ###################################################
